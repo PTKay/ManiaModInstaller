50eabab094e70c713a49a7b9c233d72b35c1c00b
PTKickass <jp_moura99@hotmail.com>
2017-09-01 16:00:05 +0100

    Update latest.bat

--- update_checker/1.0.3/latest.bat
+++ update_checker/1.0.3/latest.bat
@@ -1,7 +1,13 @@
 @echo off
 cls
-echo Version 1.0.4 is available for download!
+echo Version 1.2 is available for download!
 echo -----------------------------------------
+echo Changelog:
+echo Added an auto updater (Thanks SuperSonic16!)
+echo Made it even more user friendly, with a better GUI (?)
+echo No more separate bats, everything is under the same bat file.
+echo Automatically checks for backups. If none is found, it makes one automatically.
+echo.
 echo Download now?
 set /p downloadupdate=(Y/N)
 if /I "%downloadupdate%" EQU "Y" goto :update

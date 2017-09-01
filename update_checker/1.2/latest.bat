573123a5364e76ca8ab4dc6c2252d9312eca8e72
PTKickass <jp_moura99@hotmail.com>
2017-09-01 17:53:14 +0100

    Update latest.bat

--- update_checker/1.2/latest.bat
+++ update_checker/1.2/latest.bat
@@ -1,9 +1,9 @@
 @echo off
 cls
-echo Version 1.2.1 is available for download!
+echo Version 1.2.2 is available for download!
 echo -----------------------------------------
 echo Changelog:
-echo Fixed first 3 "del" commands
+echo Added RSDKPack.exe because I forgot to pack it in
 echo.
 echo Download now?
 set /p downloadupdate=(Y/N)
@@ -14,7 +14,7 @@ if /I "%downloadupdate%" EQU "N" goto :proceed
 cls
 echo Updating ManiaModInstaller...
 echo Don't close this window!
-powershell "($WebClient = New-Object System.Net.WebClient).DownloadFile('https://github.com/PTKickass/ManiaModInstaller/blob/master/update_checker/1.2/maniamodinstaller.7z?raw=true', 'maniamodinstaller/update.7z')"
+powershell "($WebClient = New-Object System.Net.WebClient).DownloadFile('https://github.com/PTKickass/ManiaModInstaller/blob/master/update_checker/1.2.1/maniamodinstaller.7z?raw=true', 'maniamodinstaller/update.7z')"
 
 maniamodinstaller\7z.exe x "maniamodinstaller\update.7z" -o"maniamodinstaller\update"
 

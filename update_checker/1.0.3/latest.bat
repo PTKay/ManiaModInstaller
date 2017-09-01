b4263c50975caafdbc2c2de46d90802b3d417e4a
PTKickass <jp_moura99@hotmail.com>
2017-09-01 13:16:01 +0100

    Update latest.bat

--- update_checker/1.0.3/latest.bat
+++ update_checker/1.0.3/latest.bat
@@ -1,10 +1,2 @@
 cls
-echo Your current version is 1.0.3
-
-echo The latest version is 1.0.3
-
-echo -------------------------------
-
-echo You already have the latest version installed!
-echo Press any key to proceed
-pause >nul
+echo Latest version detected

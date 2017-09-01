4e2e21f23381d94b3b06fac5448e293cc38340ed
PTKickass <jp_moura99@hotmail.com>
2017-09-01 14:43:41 +0100

    Update latest.bat

--- update_checker/1.0.3/latest.bat
+++ update_checker/1.0.3/latest.bat
@@ -1,2 +1,19 @@
+@echo off
 cls
-echo Latest version detected
+echo Version 1.0.4 is available for download!
+echo -----------------------------------------
+echo Download now?
+set /p downloadupdate=(Y/N)
+if /I "%downloadupdate%" EQU "Y" goto :update
+if /I "%downloadupdate%" EQU "N" goto :proceed
+
+:update
+cls
+echo Updating ManiaModInstaller...
+echo Don't close this window!
+powershell "($WebClient = New-Object System.Net.WebClient).DownloadFile('https://github.com/PTKickass/ManiaModInstaller/blob/master/update_checker/1.0.3/ManiaModInstaller.7z', 'ManiaModInstaller\maniamodinstaller.7z')"
+
+maniamodinstaller\7z.exe x maniamodinstaller.7z -o"maniamodinstaller\update"
+
+call maniamodinstaller\autoupdater.bat
+

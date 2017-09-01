b150fe6339061caf01ed1b23bdd956e740d64ceb
PTKickass <jp_moura99@hotmail.com>
2017-09-01 16:17:08 +0100

    Delete UpdateFileList.bat

--- UpdateFileList.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rm Data.xml /Y >nul
-echo Downloading File List...
-:: This PS script downloads sonic_mania_files_list.txt from github.com/koolkdev/rsdkv5_extract
-powershell "($WebClient = New-Object System.Net.WebClient).DownloadFile('https://github.com/koolkdev/rsdkv5_extract/blob/master/sonic_mania_files_list.txt?raw=true', 'Data.txt')"
-echo Done.

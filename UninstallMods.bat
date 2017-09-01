e48c96e347b4658b14e284e93086aa8c95b1a1e7
PTKickass <jp_moura99@hotmail.com>
2017-09-01 16:16:50 +0100

    Delete UninstallMods.bat

--- UninstallMods.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo This will uninstall any mods you currently have installed.
-echo Press any key if you want to proceed.
-pause >nul
-cls
-echo Restoring Data.rsdk from Backup folder...
-xcopy /s /y _rsdkbackup .\

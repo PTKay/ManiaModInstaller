9ca6cc439be21afb238a0773de3f96ac9b56d60a
PTKickass <jp_moura99@hotmail.com>
2017-09-01 12:08:36 +0100

    Update latest.bat

--- update_checker/1.0.3/latest.bat
+++ update_checker/1.0.3/latest.bat
@@ -3,4 +3,4 @@ echo The latest version is 1.0.3
 echo -------------------------------
 echo You have the latest version installed
 pause >nul
-call ManiaModInstaller :updater_finished
+call maniaModInstaller.bat updater_finished

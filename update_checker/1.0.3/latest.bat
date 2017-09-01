fa7086ee61876229bb4af28e537d1c18a471dc97
PTKickass <jp_moura99@hotmail.com>
2017-09-01 12:20:55 +0100

    Update latest.bat

--- update_checker/1.0.3/latest.bat
+++ update_checker/1.0.3/latest.bat
@@ -1,6 +1,12 @@
 echo Your current version is 1.0.3
+
 echo The latest version is 1.0.3
+
 echo -------------------------------
+
 echo You have the latest version installed
+
 pause >nul
-call maniaModInstaller.bat updater_finished
+
+set label=updater_finished
+call maniaModInstaller.bat %label%
